398a1c02800e3a732525f5b40e13585af4005360
Arturo García Saiza <arturogsaiza@gmail.com>
2020-10-06 09:54:58 -0700

    S13 - Creacion de una plantilla php, utilizando AdminLTE

---
+++ admin/contacto.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Home</title>
+  <?php include("includes/head.php"); ?>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+
+
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  <!-- Navbar -->
+<?php  include("includes/menu.php");  ?>
+  <!-- / navbar -->
+
+  <!-- Main Sidebar Container -->
+<?php include("includes/menu_izquierda.php"); ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Contacto</h1>
+          </div>
+           
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+<!-- desde aqui podemos cambiar todo el contenido -->
+
+
+          
+
+
+<!-- hasta aqui, podemos cambiar todo el contenido -->
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+
+
+
+
+
+
+
+  <?php include("includes/footer.php");  ?>
+   
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<?php include("includes/scripts.php"); ?>
+
+
+
+</body>
+</html>
