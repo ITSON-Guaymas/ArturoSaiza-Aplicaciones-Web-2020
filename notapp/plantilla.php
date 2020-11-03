98beac52f93b56778705c121dd212805b7ba0020
Arturo García Saiza <arturogsaiza@gmail.com>
2020-11-03 08:00:43 -0700

    plantilla para notapp

---
+++ notapp/plantilla.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Home</title>
+  <?php include("includes/head.php"); ?>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+
+
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  <!-- Navbar -->
+<?php  include("includes/menu.php");  ?>
+  <!-- / navbar -->
+
+  <!-- Main Sidebar Container -->
+<?php include("includes/menu_izquierda.php"); ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Home</h1>
+          </div>
+           
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+<!-- desde aqui podemos cambiar todo el contenido -->
+
+
+          
+
+
+<!-- hasta aqui, podemos cambiar todo el contenido -->
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+
+
+
+
+
+
+
+  <?php include("includes/footer.php");  ?>
+   
+  <!-- Control Sidebar -->
+  <?php include("includes/control_sidebar.php");  ?>
+  <!-- /.control-sidebar -->
+
+  
+</div><!-- ./wrapper -->
+
+<?php include("includes/scripts.php"); ?>
+
+
+
+</body>
+</html>
